811fcdd20417022915387649328dbc6bf2176350
Rodrigo <rodrigoyasha@hotmail.com>
2024-07-02 12:26:46 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ var num1 = 10
 var num2 = 20
 var soma = num1 + num2
 console.log(soma)
+
+
+//exercicio 2//
+var valor1 = prompt('Informe um valor')
+
+if (valor1 >= 10 ) {
+    alert(' O valor informado é maior ou igual a 10')
+} else {
+    alert('O valor ' + valor1 + ' é diferente do valor ' + valor2)
+}
+
+//exercicio 3//
